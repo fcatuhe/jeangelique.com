ec625252de04197627da23ee164445cc00e8b31c
Francois Catuhe <francois@ridecell.com>
2019-02-08 21:56:19 +0100

    update jeangelique

---
+++ src/pages/wages-familie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const WagesFamilyPage = () => (
+  <Layout>
+    <h1>Hallo Wages Familie</h1>
+    <p>Bienvenue à notre mariage</p>
+  </Layout>
+)
+
+export default WagesFamilyPage
