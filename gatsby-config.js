f98e489dc28c5bedc405ded5e825cd810201c0b7
Francois Catuhe <francois@ridecell.com>
2019-04-13 23:57:23 +0200

    add moment pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'moments',
+        path: `${__dirname}/src/pages/moments`,
+      },
+    },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-purgecss',
@@ -36,6 +43,7 @@ module.exports = {
         ],
       },
     },
+    'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
