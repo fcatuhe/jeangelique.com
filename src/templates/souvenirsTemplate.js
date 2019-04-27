b17a7e1fc4c3849a72e347dfb53ffa72c54d68e4
Francois Catuhe <francois@ridecell.com>
2019-04-27 21:00:52 +0200

    update souvernisTemplate

--- src/templates/souvenirsTemplate.js
+++ src/templates/souvenirsTemplate.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-// import Timeline from 'react-image-timeline'
-// import 'react-image-timeline/dist/timeline.css'
-
 import Layout from '../components/layout'
+import { Timeline, TimelineElement } from '../components/timeline'
 
 const SouvenirsTemplate = ({ data: { markdownRemark } }) => {
-  return <Layout />
+  const souvenirs = [markdownRemark]
+
+  return (
+    <Layout skipContainer>
+      <Timeline>
+        {souvenirs.map(souvenir => (
+          <TimelineElement
+            key={souvenir.fields.slug}
+            date={souvenir.frontmatter.date}
+          >
+            <div className="timeline-element-header">
+              <h3>{souvenir.frontmatter.title}</h3>
+              <h6 className="text-uppercase">
+                {souvenir.frontmatter.location.city}
+              </h6>
+            </div>
+            <img
+              src={
+                souvenir.frontmatter.image &&
+                `${souvenir.frontmatter.image}?nf_resize=fit&w=600`
+              }
+            />
+            {souvenir.html && (
+              <div
+                className="m-3"
+                dangerouslySetInnerHTML={{ __html: souvenir.html }}
+              />
+            )}
+          </TimelineElement>
+        ))}
+      </Timeline>
+    </Layout>
+  )
 }
 
 export default SouvenirsTemplate
@@ -15,6 +45,9 @@ export default SouvenirsTemplate
 export const souvenirsQuery = graphql`
   query($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      fields {
+        slug
+      }
       frontmatter {
         author
         title
